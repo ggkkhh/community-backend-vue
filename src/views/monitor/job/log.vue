0199bfe405998bbf7cb2e3a2e962c017edef6d66
roydon <3133010060@qq.com>
2023-05-19 17:10:09 +0800

    ui

--- src/views/monitor/job/log.vue
+++ src/views/monitor/job/log.vue
@@ -78,7 +78,7 @@
       @pagination="getList" />
 
     <!-- 调度日志详细 -->
-    <el-dialog title="调度日志详细" :visible.sync="open" width="700px" append-to-body>
+    <el-dialog title="调度日志详细" :visible.sync="open" width="70%" append-to-body>
       <el-form ref="form" :model="form" label-width="100px" size="mini">
         <el-row>
           <el-col :span="12">
