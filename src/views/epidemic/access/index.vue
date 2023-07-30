55077758260f02c5b223a6093834cb3bee7b1523
roydon <3133010060@qq.com>
2023-07-30 15:35:01 +0800

    💄 UI:

--- src/views/epidemic/access/index.vue
+++ src/views/epidemic/access/index.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="app-container">
+    <!-- 搜索栏 -->
+    <!-- realName: null,
+        telephone: null,
+        accessType: null,
+        reportType: null, -->
     <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
-      <el-form-item label="用户" prop="username">
-        <el-input v-model="queryParams.username" placeholder="请输入用户" clearable @keyup.enter.native="handleQuery" />
-      </el-form-item>
       <el-form-item label="真实姓名" prop="realName">
         <el-input v-model="queryParams.realName" placeholder="请输入真实姓名" clearable @keyup.enter.native="handleQuery" />
+      </el-form-item>
+      <el-form-item label="联系电话" prop="telephone">
+        <el-input v-model="queryParams.telephone" placeholder="请输入电话" clearable @keyup.enter.native="handleQuery" />
+      </el-form-item>
+      <el-form-item label="进出类型" prop="accessType">
+        <el-select v-model="queryParams.accessType" placeholder="进出类型" clearable style="width: 200px">
+          <el-option v-for="dict in dict.type.access_type" :key="dict.value" :label="dict.label" :value="dict.value" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="报备类型" prop="reportType">
+        <el-select v-model="queryParams.reportType" placeholder="报备类型" clearable style="width: 200px">
+          <el-option v-for="dict in dict.type.access_report_type" :key="dict.value" :label="dict.label"
+            :value="dict.value" />
+        </el-select>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
         <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
       </el-form-item>
     </el-form>
-
+    <!-- 增删查改操作栏 -->
     <el-row :gutter="10" class="mb8">
       <el-col :span="1.5">
         <el-button type="primary" plain icon="el-icon-plus" size="mini" @click="handleAdd"
@@ -32,14 +48,29 @@
       </el-col>
       <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
-
-    <el-table v-loading="loading" :data="recordList" @selection-change="handleSelectionChange">
+    <!-- 数据表格 -->
+    <el-table v-loading="loading" :data="recordList" @selection-change="handleSelectionChange" border>
       <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="用户id" align="center" prop="userId" />
-      <el-table-column label="用户" align="center" prop="username" />
+      <el-table-column label="用户账号" align="center" prop="username" />
       <el-table-column label="真实姓名" align="center" prop="realName" />
-      <el-table-column label="进出类型0进入1出" align="center" prop="accessType" />
-      <el-table-column label="出：目的地；进：来源地" align="center" prop="destination" />
+      <el-table-column label="联系电话" align="center" prop="telephone" />
+      <el-table-column label="进出类型" align="center" prop="accessType" width="80">
+        <template slot-scope="scope">
+          <dict-tag :options="dict.type.access_type" :value="scope.row.accessType" />
+        </template>
+      </el-table-column>
+      <el-table-column label="报备类型" align="center" prop="reportType" width="120">
+        <template slot-scope="scope">
+          <dict-tag :options="dict.type.access_report_type" :value="scope.row.reportType" />
+        </template>
+      </el-table-column>
+      <el-table-column label="出发地" align="center" prop="placeStart" :show-overflow-tooltip="true" />
+      <el-table-column label="目的地" align="center" prop="placeEnd" :show-overflow-tooltip="true" />
+      <el-table-column label="备注" align="center" prop="remark" :show-overflow-tooltip="true" />
+      <el-table-column label="创建人" align="center" prop="createBy" width="160" />
+      <el-table-column label="修改人" align="center" prop="updateBy" width="160" />
+      <el-table-column label="创建时间" align="center" prop="createTime" width="160" />
+      <el-table-column label="修改时间" align="center" prop="updateTime" width="160" />
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template slot-scope="scope">
           <el-button size="mini" type="text" icon="el-icon-edit" @click="handleUpdate(scope.row)"
@@ -54,19 +85,42 @@
       @pagination="getList" />
 
     <!-- 添加或修改出入社区人员记录对话框 -->
-    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
+    <el-dialog :title="title" :visible.sync="open" width="700px" append-to-body>
       <el-form ref="form" :model="form" :rules="rules" label-width="80px">
-        <el-form-item label="用户id" prop="userId">
-          <el-input v-model="form.userId" placeholder="请输入用户id" />
-        </el-form-item>
-        <el-form-item label="用户" prop="username">
-          <el-input v-model="form.username" placeholder="请输入用户" />
-        </el-form-item>
         <el-form-item label="真实姓名" prop="realName">
           <el-input v-model="form.realName" placeholder="请输入真实姓名" />
         </el-form-item>
-        <el-form-item label="出：目的地；进：来源地" prop="destination">
-          <el-input v-model="form.destination" placeholder="请输入出：目的地；进：来源地" />
+        <el-form-item label="联系电话" prop="telephone">
+          <el-input v-model="form.telephone" placeholder="请输入联系电话" />
+        </el-form-item>
+        <el-row>
+          <el-col :span="12">
+            <el-form-item label="进出类型" prop="accessType">
+              <el-select v-model="form.accessType" placeholder="请选择进出类型">
+                <el-option v-for="dict in dict.type.access_type" :key="dict.value" :label="dict.label"
+                  :value="dict.value">
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="报备类型" prop="reportType">
+              <el-select v-model="form.reportType" placeholder="请选择报备类型">
+                <el-option v-for="dict in dict.type.access_report_type" :key="dict.value" :label="dict.label"
+                  :value="dict.value">
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-form-item label="出发地" prop="placeStart">
+          <el-input v-model="form.placeStart" placeholder="请输入出发地" />
+        </el-form-item>
+        <el-form-item label="目的地" prop="placeEnd">
+          <el-input v-model="form.placeEnd" placeholder="请输入目的地" />
+        </el-form-item>
+        <el-form-item label="备注" prop="remark">
+          <el-input v-model="form.remark" placeholder="请输入备注" />
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -82,6 +136,7 @@ import { listRecord, getRecord, delRecord, addRecord, updateRecord } from "@/api
 
 export default {
   name: "Access",
+  dicts: ['sys_normal_disable', 'access_type', 'access_report_type'],
   data() {
     return {
       // 遮罩层
@@ -106,24 +161,29 @@ export default {
       queryParams: {
         pageNum: 1,
         pageSize: 10,
-        userId: null,
-        username: null,
         realName: null,
+        telephone: null,
         accessType: null,
-        destination: null,
+        reportType: null,
       },
       // 表单参数
       form: {},
       // 表单校验
       rules: {
-        userId: [
-          { required: true, message: "用户id不能为空", trigger: "blur" }
+        realName: [
+          { required: true, message: "真实姓名不能为空", trigger: "blur" },
+          { min: 2, max: 10, message: '真实姓名长度必须介于 2 和 10 之间', trigger: 'blur' }
         ],
-        username: [
-          { required: true, message: "用户不能为空", trigger: "blur" }
+        telephone: [
+          {
+            pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/, message: "请输入正确的手机号码", trigger: "blur"
+          }
         ],
         accessType: [
-          { required: true, message: "进出类型0进入1出不能为空", trigger: "change" }
+          { required: true, message: "进出类型不能为空", trigger: "change" }
+        ],
+        reportType: [
+          { required: true, message: "报备类型不能为空", trigger: "change" }
         ]
       }
     };
@@ -150,16 +210,13 @@ export default {
     reset() {
       this.form = {
         recordId: null,
-        userId: null,
-        username: null,
         realName: null,
-        accessType: null,
-        destination: null,
-        delFlag: null,
-        createTime: null,
-        updateTime: null,
-        createBy: null,
-        updateBy: null
+        telephone: null,
+        accessType: '0',
+        reportType: '1',
+        placeStart: null,
+        placeEnd: null,
+        remark: null
       };
       this.resetForm("form");
     },
