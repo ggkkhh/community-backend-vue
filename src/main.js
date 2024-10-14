0aa5aa1c6d75972a8fc817f5c6c0248e4a3bfadb
roydon <3133010060@qq.com>
2024-10-14 21:30:34 +0800

    default size: medium

--- src/main.js
+++ src/main.js
@@ -121,7 +121,7 @@ DictData.install()
  */
 
 Vue.use(Element, {
-  size: Cookies.get('size') || 'small' // set element-ui default size
+  size: Cookies.get('size') || 'medium' // set element-ui default size
 })
 
 Vue.use(Viewer)
